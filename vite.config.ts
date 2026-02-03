7d2b2111013b602470621f5a1891e30bc3dae1c1
Frank shen <frank.shen@newpos.tw>
2026-02-04 00:13:59 +0800

    fix: add build target for mobile browser compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,7 @@ export default defineConfig(({ mode }) => {
         }
       },
       build: {
+        target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14'],
         rollupOptions: {
           output: {
             manualChunks: {
